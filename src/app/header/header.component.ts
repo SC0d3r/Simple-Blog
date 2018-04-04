7634dbcd0ea0005856c46a44d80fef1a99272509
SC0d3r <sasan.kelishani@gmail.com>
2018-04-04 19:10:42 +0430

    add nav icon for small screens

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  isSmallScreen : boolean;
   constructor() { }
 
   ngOnInit() {
+    this.isSmallScreen = true;
   }
-
+  onResize(e){
+    const width = e.target.outerWidth;
+    console.log(width);
+    if(width <= 800){
+      this.isSmallScreen = true;
+    }else {
+      this.isSmallScreen = false;
+    }
+  }
 }
