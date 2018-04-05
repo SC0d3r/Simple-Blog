4b2f0680d65fc4d7612f5eae861be670b2e97d3f
SC0d3r <sasan.kelishani@gmail.com>
2018-04-05 20:36:05 +0430

    add new insights,projects,top-footer components to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,26 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
-import { BoardComponent } from './board/board.component';
+import { TopFooterComponent } from './top-footer/top-footer.component';
+import { InsightsComponent } from './insights/insights.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FixedBoardComponent } from './fixed-board/fixed-board.component';
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import { FixedBoardComponent } from './fixed-board/fixed-board.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent,
     FooterComponent,
-    BoardComponent,
-    FixedBoardComponent
+    FixedBoardComponent,
+    InsightsComponent,
+    TopFooterComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
