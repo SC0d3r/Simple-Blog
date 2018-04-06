2b5280e551719123bc8350ff319dbe3fc0a6c9ad
SC0d3r <sasan.kelishani@gmail.com>
2018-04-06 15:47:28 +0430

    add spme dummyProjects

---
+++ src/app/projects/dummyProjects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: 'Coding with Clarity',
+    description: `Always writing the clearest possible code is what helps separate the great developers from the merely good. It cuts confusion, reduces headaches `,
+    img: 'js.png',
+    tags: []
+  },
+  {
+    title: 'JavaScript for Web Designers: DOM Scripting',
+    description: 'JavaScript’s ability to access and modify elements on the page is undeniable, and the Document Object Model is an essential component of that',
+    img: 'js2.svg',
+    tags: []
+  },
+  {
+    title: 'Interaction Is an Enhancement',
+    description: 'The web has an endless supply of single-point-of-failure horror stories, where sites serve up blank and blocked pages to dumbstruck users.',
+    img: 'js3.jpg',
+    tags: []
+  }
+];
