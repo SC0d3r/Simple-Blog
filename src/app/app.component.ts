6c1b7b7459b9149075672a4b021ddf77bc936fc5
SC0d3r <sasan.kelishani@gmail.com>
2018-04-08 01:06:01 +0430

    move slide feature to index.html instead of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, HostListener } from '@angular/core';
+import { Component} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  @HostListener('window:wheel', ['$event'])
-  onScrollEvent($event) {
-    slide($event.deltaY < 0 , 15);
-  }
-}
+export class AppComponent {}
 
-
-function slide(shouldGoUp , howManyTimes){
-  if (howManyTimes <= 0) return;
-  
-  const amountToScroll = (shouldGoUp ? -1 : 1) * 8;
-  window.scrollTo(0,window.scrollY + amountToScroll);
-
-  window.requestAnimationFrame(() => slide(shouldGoUp,howManyTimes-1));
-}
