d66beaa34761b7002e655c7919b296f0401ad06c
SC0d3r <sasan.kelishani@gmail.com>
2018-04-08 03:18:32 +0430

    add some text to dummy articles

--- src/app/services/dummyArticles.ts
+++ src/app/services/dummyArticles.ts
@@ -3,20 +3,67 @@ export const dummyArticles = [
     id : 'aswe13213fsdsd',
     title: 'Coding with Clarity',
     body: `
-    Imagine you’re working on a home project and you pick up a drill to drive a screw into a wall. When you pull the drill away from the screw, you discover that this drill has an interesting feature: it squirts a quick-drying drywall compound over the driven screw to hide it. Well, that’s great if you want to paint over the screw, but that’s not always the case. You shouldn’t have to get a second drill just to drill a hole in something. The drill would be much more usable and reliable if it just did one thing, and it would also be flexible enough to use in a variety of situations.
+
+    ![asdfds](/assets/images/js2.svg)
+
+Working code isn’t necessarily good code. 
+Your code also needs to be easy to read, understand, and modify. It needs clarity, and to achieve that, it has to be organized well, with careful planning and proper separation of ideas taking place before you even open your code editor. Coding for clarity is something that separates the great developers from the merely good, and there are a few basic principles that can set you on that path.
+
+<br>
+
+## The single responsibility principle
+<br>
+  Imagine you’re working on a home project and you pick up a drill to drive a screw into a wall. When you pull the drill away from the screw, you discover that this drill has an interesting feature: it squirts a quick-drying drywall compound over the driven screw to hide it. Well, that’s great if you want to paint over the screw, but that’s not always the case. You shouldn’t have to get a second drill just to drill a hole in something. The drill would be much more usable and reliable if it just did one thing, and it would also be flexible enough to use in a variety of situations.
 
 The single responsibility principle states that a block of code should do one thing, and do it well. Like the drill above, limiting its functionality actually increases the usefulness of a block of code. Coding this way not only saves you a lot of headache, but it will save future developers on the project a lot of headache as well.
 
-Think of functions and methods in terms of responsibilities. As you increase its responsibilities, a block of code becomes less flexible and reliable, more demanding of changes, and more susceptible to errors. For the most clarity, each function or method should have one responsibility.
+Think of functions and methods in terms of responsibilities. 
+\`\`\`javascript
+function getFirstName() {
+  var firstName = document.querySelector("#firstName").value;
+  firstName = firstName.toLowerCase();
+  setCookie("firstName", firstName);
+  if (firstName === null) {
+      return "";
+  }
+  return firstName;
+}
 
+var activeFirstName = getFirstName();
+\`\`\`
+As you increase its responsibilities, a block of code becomes less flexible and reliable, more demanding of changes, and more susceptible to errors. For the most clarity, each function or method should have one responsibility.
+  
 If you’re describing what a function does and you have to use the word “and,” that function is probably too complex. What a function does should be simple enough to explain with only a descriptive function name and descriptive arguments.
-
+  
 I was tasked recently with creating an electronic version of the Myers-Briggs personality test. I’d done this before, and when I first approached the problem a few years ago, I coded one giant function called processForm—it gathered the scores, generated the charts, and took care of everything in the DOM to display things to the user.
-
+  
 The problem was that if anything had to change, you had to search through a mountain of code to figure out where to make the alteration. Also, if something went wrong in the middle of the function, it was a lot harder to find the error.
+  
+So when facing the problem this time, I broke everything down into single-responsibility functions wrapped up in a module object instead. The resulting function called upon form submission looked like this:
+\`\`\`javascript
+    return {
+        processForm: function() {
+            getScores();
+            calculatePercentages();
+            createCharts();
+            showResults();
+        }
+    };    
+\`\`\`
+Remember, code \`should be flexible\` enough to cover a wide variety of use cases. If you find yourself copying and pasting code and making minor changes, or rewriting code because code changed somewhere else, this is tight coupling in action. (For example, to make the getFirstName function from earlier reusable, you could replace the hard-coded firstName with a generic ID passed to the function.) Other signs of this include hard-coded IDs in functions, too many function parameters, multiple similar functions, and large functions that violate the single responsibility principle.
+\`\`\`javascript
+var CollegeCourse = (function() {
+  function createStudent_WRONG(firstName, lastName, studentID) {
+    //adfdasf  
+    /* dfsf*/
+  }
 
-So when facing the problem this time, I broke everything down into single-responsibility functions wrapped up in a module object instead.
-    `,
+  function createStudent_RIGHT(optionsObject) {
+  }
+}());
+\`\`\`
+You shouldn’t have to modify a class because another class changes. This is a classic case of tight coupling. Constructor parameters can be passed as an object with the receiving object having fallback default values, which loosens coupling and means code won’t break when you add new parameters.
+`,
     img: { src: 'js.png', alt: '' },
     tags: []
   },
