496a6a6148bf45fd6547d2e0c40f1411377be507
SC0d3r <sasan.kelishani@gmail.com>
2018-04-07 18:31:11 +0430

    add navigation to home button

--- src/app/article-left-side/article-left-side.component.ts
+++ src/app/article-left-side/article-left-side.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleLeftSideComponent implements OnInit {
 
-  constructor() { }
+  constructor(private  _router : Router) { }
 
   ngOnInit() {
   }
-
+  goHomePage(){
+    this._router.navigate(['/']);
+  }
 }
