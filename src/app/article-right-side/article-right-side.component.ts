ba91d1d2fad37efe8cde2a76aaef2d767e196932
SC0d3r <sasan.kelishani@gmail.com>
2018-04-08 03:18:11 +0430

    (article right side) : adjust image size with markdown service

--- src/app/article-right-side/article-right-side.component.ts
+++ src/app/article-right-side/article-right-side.component.ts
@@ -2,6 +2,8 @@ import { ActivatedRoute } from '@angular/router';
 import { ArticlesInfoService } from './../services/articles-info.service';
 import { Component, OnInit } from '@angular/core';
 
+import { MarkdownService } from 'angular2-markdown';
+
 @Component({
   selector: 'app-article-right-side',
   templateUrl: './article-right-side.component.html',
@@ -10,13 +12,19 @@ import { Component, OnInit } from '@angular/core';
 export class ArticleRightSideComponent implements OnInit {
   article: any;
   constructor(private _aritcleInfo: ArticlesInfoService,
-    private _route: ActivatedRoute) {
+    private _route: ActivatedRoute,
+    private _markdown: MarkdownService
+  ) {
     const articleID = this._route.snapshot.paramMap.get('id');
     this._aritcleInfo.fetchArticleByID(articleID)
       .subscribe(art => this.article = art);
   }
 
   ngOnInit() {
+    this._markdown.renderer.image = (src: string) => {
+      // console.log(img);
+      return `<img src = "${src}" style = "/*border : 1px solid red;*/width : 80%;display : block;margin:5% auto;">`;
+    }
   }
 
 }
