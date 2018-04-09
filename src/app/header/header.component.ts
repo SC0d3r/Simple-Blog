76ebaff6cad35c1727ae9b2c745cd2c49729c578
SC0d3r <sasan.kelishani@gmail.com>
2018-04-09 22:23:04 +0430

    add header transition for 'about me'

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isSmallScreen : boolean;
+  @ViewChild('avatar') private _avatar : ElementRef;
+  @ViewChild('writerName') private _writerName : ElementRef;
+  @ViewChild('header') private _header : ElementRef;
+  @ViewChild('aboutMe') private _aboutMeText : ElementRef;
+  @ViewChild('seprator') private _seprator : ElementRef;
+  @ViewChild('links') private _links : ElementRef;
+  @ViewChild('icon') private _icon : ElementRef;
+  @ViewChild('bgImage') private _bgImage : ElementRef;
+  @ViewChild('cancelTranstion') private _cancelTranstion : ElementRef;
+
   constructor() { }
 
   ngOnInit() {
@@ -17,7 +27,7 @@ export class HeaderComponent implements OnInit {
     // console.log(width);
     this._showOrRemoveLinks(width);
   }
-
+  
   private _showOrRemoveLinks(width: any) {
     if (width <= 800) {
       this.isSmallScreen = true;
@@ -26,5 +36,50 @@ export class HeaderComponent implements OnInit {
       this.isSmallScreen = false;
     }
   }
+  
+  
+  openAboutMe(){
+    const avatarDiv : HTMLDivElement= this._avatar.nativeElement;
+    const writerName : HTMLDivElement= this._writerName.nativeElement;
+    const header : HTMLDivElement= this._header.nativeElement;
+    const aboutMeText : HTMLDivElement= this._aboutMeText.nativeElement;
+    const seprator : HTMLDivElement= this._seprator.nativeElement;
+    const links : HTMLDivElement= this._links.nativeElement;
+    const icon : HTMLDivElement= this._icon.nativeElement;
+    const bgImage : HTMLDivElement= this._bgImage.nativeElement;
+    const cancelTranstion : HTMLDivElement= this._cancelTranstion.nativeElement;
 
+    avatarDiv.classList.add('avatar-transition');
+    writerName.classList.add('writerName-transition');
+    header.classList.add('header-transition');
+    aboutMeText.classList.add('aboutMeText-transition');
+    seprator.classList.add('zero-opacity');
+    links.classList.add('zero-opacity');
+    icon.classList.add('zero-opacity');
+    bgImage.classList.add('bgImage-transition');
+    cancelTranstion.classList.add('cancelTransition-transition');
+    // cancelTranstion
+  }
+
+  cancelTransition(){
+    const avatarDiv : HTMLDivElement= this._avatar.nativeElement;
+    const writerName : HTMLDivElement= this._writerName.nativeElement;
+    const header : HTMLDivElement= this._header.nativeElement;
+    const aboutMeText : HTMLDivElement= this._aboutMeText.nativeElement;
+    const seprator : HTMLDivElement= this._seprator.nativeElement;
+    const links : HTMLDivElement= this._links.nativeElement;
+    const icon : HTMLDivElement= this._icon.nativeElement;
+    const bgImage : HTMLDivElement= this._bgImage.nativeElement;
+    const cancelTranstion : HTMLDivElement= this._cancelTranstion.nativeElement;
+
+    avatarDiv.classList.remove('avatar-transition');
+    writerName.classList.remove('writerName-transition');
+    header.classList.remove('header-transition');
+    aboutMeText.classList.remove('aboutMeText-transition');
+    seprator.classList.remove('zero-opacity');
+    links.classList.remove('zero-opacity');
+    icon.classList.remove('zero-opacity');
+    bgImage.classList.remove('bgImage-transition');
+    cancelTranstion.classList.remove('cancelTransition-transition');
+  }
 }
