bc9bfa9826552f28338c23a693042563e689dd65
SC0d3r <sasan.kelishani@gmail.com>
2018-04-14 02:36:42 +0430

    add smooth scrolling to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 
@@ -7,15 +7,31 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(@Inject(PLATFORM_ID) plafromID : any,
-private _route : Router){
-    if(isPlatformBrowser(plafromID)){
-      const GridNotSupported = !CSS.supports('display' , 'grid');
-      if(GridNotSupported){
+export class AppComponent implements OnInit {
+  constructor(
+    @Inject(PLATFORM_ID) public plafromID: any,
+    private _route: Router) {
+    if (isPlatformBrowser(plafromID)) {
+      const GridNotSupported = !CSS.supports('display', 'grid');
+      if (GridNotSupported) {
         this._route.navigate(['/notSupported']);
       }
     }
   }
-}
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.plafromID)) {
+      window.addEventListener('wheel', event => {
+        slide(window, event.deltaY < 0, 15);
+      });
+    }
+  }
 
+}
+function slide(win, shouldGoUp, howManyTimes) {
+  if (howManyTimes <= 0) return;
+
+  const amountToScroll = (shouldGoUp ? -1 : 1) * 8;
+  win.scrollTo(0, win.scrollY + amountToScroll);
+
+  win.requestAnimationFrame(() => slide(win, shouldGoUp, howManyTimes - 1));
+}
