6e1fec56c71f1d997200b8b13ef78a14afa5c760
SC0d3r <sasan.kelishani@gmail.com>
2018-04-14 02:39:03 +0430

    use database service in article-info service

--- src/app/services/articles-info.service.ts
+++ src/app/services/articles-info.service.ts
@@ -1,25 +1,33 @@
+import { DatabaseService } from './db/database.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { dummyArticles } from './dummyArticles';
 import { Article } from './Article';
+import { DB } from './db/DB';
 
 @Injectable()
 export class ArticlesInfoService {
-  articles : Article[];
-  constructor() {
+  articles: Article[];
+  constructor(private _db: DatabaseService) {
     this.articles = dummyArticles;
   }
 
-  fetchArticles(howMany : number = -1) : Observable<Article[]>{
+  fetchArticles(howMany: number = -1): Observable<Article[]> {
     // -1 means all the articles
     return of(this.articles);
   }
 
-  fetchArticleByID(id : string) : Observable<Article>{
+  fetchArticleByID(id: string): Observable<Article> {
     const article = this.articles.filter(art => art.id === id);
-
     return of(article[0]);
+  }
+
+  hasIPVoted(articleID: string): Promise<Object> {
+    return this._db.hasIPVoted(articleID);
+  }
+  saveVote(articleID: string, vote: 'like' | 'dislike') {
+    return this._db.saveVote(articleID, vote);
   }
 
 }
