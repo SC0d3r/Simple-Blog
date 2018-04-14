38a91cc00aa8b4e07730a0406152318481d4d22c
SC0d3r <sasan.kelishani@gmail.com>
2018-04-14 22:43:17 +0430

    add interface VotesDB

---
+++ server/db/VotesDB.ts
@@ -0,0 +1,12 @@
+export type Vote = 'like' | 'dislike';
+export interface IpVoteMap{
+  [key:string] : Vote;
+}
+interface ArticleVote {
+  [key:string] : IpVoteMap
+}
+
+export interface VotesDB {
+  saveVote(articleID : string , ip : string , vote : Vote) : Promise<boolean>;
+  getVotes(articleID : string) : Promise<IpVoteMap>;
+}
