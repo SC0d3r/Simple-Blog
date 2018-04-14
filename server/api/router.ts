62cb374e1724481df13d6d8fd0ac58843458ac2e
SC0d3r <sasan.kelishani@gmail.com>
2018-04-14 22:44:34 +0430

    use VotesDB to save and retrieve votes in api/router

--- server/api/router.ts
+++ server/api/router.ts
@@ -1,3 +1,4 @@
+import { VotesDBImp } from '../db/VotesDBImp';
 import { Router } from 'express';
 
 export const router = Router();
@@ -5,24 +6,41 @@ export const router = Router();
 const PORT = process.env.PORT || 4000;
 const hostName = process.env.host || `http://localhost:${PORT}`;
 
-// router.post('/votes', (req, res) => {
-  
-  
-// });
+const votesDB = new VotesDBImp();
+
 router.post('/votes/checkIP', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  console.log(req.body);
-  console.log(`ip address of client is ${ip}`);
-  
-  //TODO : read vote from database
-  res.json({voted : true});
-});
-router.post('/votes/saveVote', (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  //save vote to database
+  const ip = getClientIPAddress(req);
+
+  // console.log(req.body);
+  // console.log(`ip address of client is ${ip}`);
+
+  const { articleID } = req.body;
+  votesDB.getVotes(articleID).then(data => {
+    // console.log('getting votes from db');
+    if (data) {
+      // console.log(data);
+      if (data[<string>ip]) {
+        return res.json({ voted: true });
+      }
+    }
+    res.json({ voted: false });
+  });
 });
 
-router.get('*' , (req ,res) => {
+router.post('/votes/saveVote', (req, res) => {
+  const ip = getClientIPAddress(req);
+  const { articleID, vote } = req.body;
+  // console.log(`saving vote ${vote} for article ${articleID} and ip ${ip}`);
+  //TODO : sanitizing
+  votesDB.saveVote(articleID, <string>ip, vote);
+});
+
+router.get('*', (req, res) => {
   res.redirect(hostName);
 });
 
+
+function getClientIPAddress(req) {
+  return (req.headers['x-forwarded-for'] || '').split(',')[0] 
+      || req.connection.remoteAddress;
+};
