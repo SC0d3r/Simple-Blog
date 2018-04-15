b94110e49562a47f3c8621e56d21e0e64f4faa2d
SC0d3r <sasan.kelishani@gmail.com>
2018-04-16 00:35:44 +0430

    add image upload facility to admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,6 @@
 import { ArticlesInfoService } from './../services/articles-info.service';
 import { Article } from './../services/Article';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
@@ -12,35 +12,45 @@ export class AdminComponent implements OnInit {
   shortDescription: string = '';
   body: string = '';
   tags: string = '';
-  img: string = '';
+  @ViewChild('file') private _file: ElementRef;
+
   constructor(private _articleInfo: ArticlesInfoService) { }
 
   ngOnInit() {
     // TODO : editin article
   }
-
+  
   onPost() {
-    //TODO : create a article and save it do database
+    const fileElement: HTMLInputElement = this._file.nativeElement;
+    const imageFile = fileElement.files[0];
+    console.log(imageFile);
+
+    if (imageFile === undefined || fileSizeExceeds1MG(imageFile.size)) {
+      console.log('select an image or file exceeds the 1mg limit');
+      return;
+    }
+
     if (notValid(this.body) ||
       notValid(this.title) ||
       notValid(this.tags) ||
-      notValid(this.shortDescription) /*||
-  notValid(this.img)*/) {
+      notValid(this.shortDescription)) {
       return;
     }
+
+    // TODO : Sanitize image file name
     const newArticle = createArticle(
-      this.img,
+      imageFile.name,
       this.title,
       this.shortDescription,
       this.body,
       this.tags);
 
     this._articleInfo.saveArticle(newArticle);
+    this._articleInfo.uploadImage(imageFile);
     this._reset();
   }
   private _reset() {
     this.title = '';
-    this.img = '';
     this.shortDescription = '';
     this.body = '';
     this.tags = '';
@@ -72,4 +82,10 @@ function createArticle(img, title, shortDesc, body, tags) {
 function guid() {
   return Math.random().toString(36).substring(2)
     + (new Date()).getTime().toString(36);
+}
+
+function fileSizeExceeds1MG(size : number){
+  size /= 1000;//turn into kb
+  size /= 1000;//turn into mg
+  return size > 1;
 }
