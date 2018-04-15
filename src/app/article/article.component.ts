b08dcbd19891b696fee3dd2ecaafa55c39971420
SC0d3r <sasan.kelishani@gmail.com>
2018-04-16 03:28:24 +0430

    article component : add fetching article from article-info service

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,6 +1,8 @@
 import { DOCUMENT } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit, Inject } from '@angular/core';
+import { ArticlesInfoService } from '../services/articles-info.service';
+import { Article } from '../services/Article';
 
 @Component({
   selector: 'app-article',
@@ -8,17 +10,23 @@ import { Component, OnInit, Inject } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-
+  article: Article;
   constructor(
     @Inject(DOCUMENT) private _document : Document,
     private _route : ActivatedRoute,
+    private _aritcleInfo: ArticlesInfoService,
   private _router : Router) { }
 
   ngOnInit() {
+    const articleID = this._route.snapshot.paramMap.get('id');
+    this._aritcleInfo.fetchArticleByID(articleID)
+    .subscribe(art => this.article = art);
+
     const isSelected = this._route.snapshot.queryParamMap.get('selected');
     if(isSelected) this._document.documentElement.scrollTo(0,0);
     // this line if for clearing the query params from page url
     this._router.navigate(['.'], { relativeTo: this._route, queryParams: {} });
+
   }
 
 }
