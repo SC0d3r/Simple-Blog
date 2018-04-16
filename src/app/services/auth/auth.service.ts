a44e9fc488dc1a9c9a4200f6bdd2948d3cf89ef7
SC0d3r <sasan.kelishani@gmail.com>
2018-04-16 21:48:57 +0430

    implement login method on auth service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,20 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../environments/environment';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/do';
+import * as hash from 'hash.js';
+
+const authURL = environment.host + '/auth';
 
 @Injectable()
 export class AuthService {
   isLoggedIn = false;
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
 
   // store the URL so we can redirect after logging in
   redirectUrl: string;
 
-  login(): Observable<boolean> {
-    //TODO : send hash the pwd and send post request to authenticate
-    return Observable.of(true).delay(1000).do(val => this.isLoggedIn = true);
+  login(username: string, password: string): Promise<boolean> {
+    const p = (<any>hash).sha256().update(password).digest('hex');
+    return this._httpClient.get(authURL).toPromise().then(data => {
+      const hash = (<any>data).auth;
+      const isOk = hash === p;
+      this.isLoggedIn = isOk;
+      return isOk;
+    });
   }
 
   logout(): void {
