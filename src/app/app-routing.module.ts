a16fd29a39d7075c5a1444e0e3b0c4b414ce08d6
SC0d3r <sasan.kelishani@gmail.com>
2018-04-16 17:34:22 +0430

    add login to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotSupportedComponent } from './not-supported/not-supported.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -8,6 +9,7 @@ import {RouterModule , Routes} from '@angular/router';
 const routes : Routes = [
   {path:'' ,pathMatch : 'full',component : HomePageComponent},
   {path : 'article/:id' , component : ArticleComponent},
+  {path : 'login' , component : LoginComponent},
   {path : 'admin' , component : AdminComponent},
   {path : 'notSupported' , component : NotSupportedComponent},
   {path : '**' , redirectTo : ''}
