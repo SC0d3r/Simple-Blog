5ebd199d25345aea6f057be4248a440d60464407
SC0d3r <sasan.kelishani@gmail.com>
2018-04-17 19:56:27 +0430

    update delArticle method in articelsDBImp to delete image files of article

--- server/db/articles/ArticlesDBImp.ts
+++ server/db/articles/ArticlesDBImp.ts
@@ -1,13 +1,13 @@
 import { ArticlesDB } from './ArticlesDB';
 import { Article } from '../../../src/app/services/Article';
-import { resolve } from 'path';
-
+import { resolve, join, basename } from 'path';
+import { unlink } from 'fs';
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync(resolve(process.cwd(), 'server', 'db', 'articles', './db.json'));
 
 export class ArticlesDBImp implements ArticlesDB {
-  
+
   private _db: any;
   constructor() {
     this._db = low(adapter);
@@ -20,9 +20,31 @@ export class ArticlesDBImp implements ArticlesDB {
     //TODO : sanitizing
     this._db.get('articles').push(article).write();
   }
-  
+
   delArticle(articleID: string): Promise<boolean> {
-    throw new Error("Method not implemented.");
+    const res: [Article] | void[] = this._db.get('articles')
+      .remove({ id: articleID })
+      .write();
+    if (res.length === 0) return Promise.resolve(false);
+
+    const removedArticle = <Article>res[0];
+    const removablePictures = [removedArticle.img.src]
+      .concat(removedArticle.innerBodyImages);
+
+    const uploadsDir = resolve(
+      process.cwd(), 'dist',
+      'browser', 'assets',
+      'images', 'uploads');
+
+    removablePictures.forEach(imageName => {
+      const imagePath = join(uploadsDir , basename(imageName));
+      unlink(imagePath, function (err) {
+        if (err) return console.log(err);
+        console.log(`image ${basename(imageName)} deleted successfully!`);
+      });
+    });
+
+    return Promise.resolve(true);
   }
 
   fetchArticles(howMany: number = -1): Promise<Article[]> {
