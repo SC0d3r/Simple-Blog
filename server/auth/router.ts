86b375f6e88bb4a7cbee7b2da5e0b665f7c4cfc4
SC0d3r <sasan.kelishani@gmail.com>
2018-04-17 21:46:04 +0430

    update hash name for serve login authentication

--- server/auth/router.ts
+++ server/auth/router.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 
 export const router = Router();
 const userAuth = require('./admin-user.json');
-const { authPwd, username } = userAuth;
+const {authHash} = userAuth;
 
 const PORT = process.env.PORT || 4000;
 const hostName = process.env.host || `http://localhost:${PORT}`;
 
 router.get('/', (req, res) => {
-  return res.json({ auth: authPwd });
+  return res.json({ auth: authHash });
 });
 
 router.get('*', (req, res) => {
