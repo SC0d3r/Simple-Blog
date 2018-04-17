65d55210cfd4051ea618cb8b321025e4dc5f2f63
SC0d3r <sasan.kelishani@gmail.com>
2018-04-17 19:57:56 +0430

    update dleArticle in database service to delete articles votes too

--- src/app/services/db/database.service.ts
+++ src/app/services/db/database.service.ts
@@ -20,8 +20,8 @@ export class DatabaseService {
         'Content-Type': 'application/json'
       })
     };
-    return this._httpClient.post(votesURL+'/checkIP',
-      { articleID },httpOptions)
+    return this._httpClient.post(votesURL + '/checkIP',
+      { articleID }, httpOptions)
       .toPromise();
   }
   saveVote(articleID: string, vote: "like" | "dislike"): Promise<Object> {
@@ -30,26 +30,48 @@ export class DatabaseService {
         'Content-Type': 'application/json'
       })
     };
-    return this._httpClient.post(votesURL+'/saveVote',
-      { articleID, vote},httpOptions)
+    return this._httpClient.post(votesURL + '/saveVote',
+      { articleID, vote }, httpOptions)
       .toPromise();
   }
 
   //articles
-  saveArticle(article : Article){
+  saveArticle(article: Article) {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
       })
     };
-    this._httpClient.post(articlesURL+'/save',{article},httpOptions).toPromise();
+    this._httpClient.post(articlesURL + '/save', { article }, httpOptions).toPromise();
   }
-  fetchArticles(howMany = -1){
-    // -1 means all
-    return this._httpClient.get<Article[]>(articlesURL+'/'+howMany);
+  delArticle(articleID: string): Promise<{ isDeleted: boolean }> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    const delArticle = this._httpClient.post(
+      articlesURL + "/delete", { articleID }, httpOptions
+    ).toPromise();
+    const delArtilceVotes = this._httpClient.post(
+      votesURL + '/delVotes', { articleID }, httpOptions
+    ).toPromise();
+    return Promise.all([delArticle, delArtilceVotes]).then(
+      results => {
+        const [isArticleDeleted, isArtilceVotesDeleted] = results;
+        return {
+          isDeleted: (<{ isDeleted: boolean }>isArticleDeleted).isDeleted &&
+          (<{ isDeleted: boolean }>isArtilceVotesDeleted).isDeleted
+        };
+      });
   }
 
-  uploadImage(formData : FormData){
-    this._httpClient.post(articlesURL+'/image',formData).toPromise();
+  fetchArticles(howMany = -1) {
+    // -1 means all
+    return this._httpClient.get<Article[]>(articlesURL + '/' + howMany);
+  }
+
+  uploadImage(formData: FormData) {
+    this._httpClient.post(articlesURL + '/image', formData).toPromise();
   }
 }
