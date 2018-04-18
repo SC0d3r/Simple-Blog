09e01db450f6fad93f36edfeb1b01d175146df75
SC0d3r <sasan.kelishani@gmail.com>
2018-04-18 22:46:53 +0430

    add more typing text to fixed-board

--- src/app/fixed-board/fixed-board.component.ts
+++ src/app/fixed-board/fixed-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-fixed-board',
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fixed-board.component.css']
 })
 export class FixedBoardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  shouldStart : boolean;
+  shouldStart2 : boolean;
+  @ViewChild('spa') spa: ElementRef;
+  @ViewChild('span2') span2: ElementRef;
+  constructor() {
+    this.shouldStart = false;
+    this.shouldStart2 = false;
   }
 
+  ngOnInit() {
+    const span: HTMLSpanElement = this.spa.nativeElement;
+    const span2 : HTMLSpanElement = this.span2.nativeElement;
+    span.addEventListener(whichAnimationEvent(), event => {
+      span.style.display = 'none';
+      this.shouldStart = true;
+    });
+    span2.addEventListener(whichAnimationEvent(), event => {
+      span2.style.display = 'none';
+      this.shouldStart2 = true;
+    });
+  }
+
+}
+
+function whichAnimationEvent() {
+  var t,
+    el = document.createElement("fakeelement");
+
+  var animations = {
+    "animation": "animationend",
+    "OAnimation": "oAnimationEnd",
+    "MozAnimation": "animationend",
+    "WebkitAnimation": "webkitAnimationEnd"
+  }
+
+  for (t in animations) {
+    if (el.style[t] !== undefined) {
+      return animations[t];
+    }
+  }
 }
