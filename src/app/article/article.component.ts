0da0f98edf408d228728196a6041cb0c5bce623e
SC0d3r <sasan.kelishani@gmail.com>
2018-04-23 18:15:27 +0430

    Update article component : fetches article views and pass it to child components

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,3 +1,4 @@
+import { ViewsService } from './../services/views/views.service';
 import { DOCUMENT } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit, Inject } from '@angular/core';
@@ -11,24 +12,31 @@ import { Article } from '../services/Article';
 })
 export class ArticleComponent implements OnInit {
   article: Article;
+  views: number;
   constructor(
-    @Inject(DOCUMENT) private _document : Document,
-    private _route : ActivatedRoute,
+    @Inject(DOCUMENT) private _document: Document,
+    private _route: ActivatedRoute,
     private _aritcleInfo: ArticlesInfoService,
-  private _router : Router) { }
+    private _views: ViewsService,
+    private _router: Router) { }
 
   ngOnInit() {
     const articleID = this._route.snapshot.paramMap.get('id');
     this._aritcleInfo.fetchArticleByID(articleID)
-    .subscribe(art => {
-      if(art) this.article = art;
-      else {
-        this._router.navigate(['/']);
-      }
-    });
+      .subscribe(art => {
+        if (art) {
+          this.article = art;
+          this._views.increaseArticleView(articleID).then((result) => {
+            this.views = (result && result.visits) || 0;
+          });
+        }
+        else {
+          this._router.navigate(['/']);
+        }
+      });
 
     const isSelected = this._route.snapshot.queryParamMap.get('selected');
-    if(isSelected) this._document.documentElement.scrollTo(0,0);
+    if (isSelected) this._document.documentElement.scrollTo(0, 0);
     // this line if for clearing the query params from page url
     this._router.navigate(['.'], { relativeTo: this._route, queryParams: {} });
 
